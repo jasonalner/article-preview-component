f85ba88aa495e04d92df52950f9cbfc0c887e683
usermane66 <68332445+usermane66@users.noreply.github.com>
2021-01-19 12:42:42 +0000

    adding javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+function showShare() {
+    let x = document.getElementById("social-banner");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none"
+    }
+    
+}
